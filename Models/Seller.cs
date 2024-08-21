f11a1a25b05876182a336a119f21f706a5be47be
Ross Morgan <ross.morgan933@gmail.com>
2024-08-21 12:43:51 -0500

    created models for seller and product

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -2,5 +2,10 @@
 {
     public class Seller
     {
+        public int SellerId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
     }
 }
