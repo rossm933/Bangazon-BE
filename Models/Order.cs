eed78948e8d3ea50c4168becf11f314bdd1fb36b
Ross Morgan <ross.morgan933@gmail.com>
2024-08-21 12:59:48 -0500

    created model for order

--- Models/Order.cs
+++ Models/Order.cs
@@ -2,5 +2,13 @@
 {
     public class Order
     {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal totalAmount { get; set; }
+        public int UserId { get; set; }
+        public string? PaymentType { get; set; }
+        public bool Status { get; set; }
+
+
     }
 }
